7571e56a3d592a5b7b54345307eb7294ccdb00b6
Амина Кадралинова <Tyuiu.KadralinovaAT@outlook.com>
2025-12-17 17:13:32 +0500

    Работа с библиотекой и тестом

--- Tyuiu.KadralinovaAT.Sprint7.Project.V7.Lib/DataService.cs
+++ Tyuiu.KadralinovaAT.Sprint7.Project.V7.Lib/DataService.cs
@@ -2,6 +2,42 @@
 {
     public class DataService
     {
+        public string[,] GetArray(string path)
+        {
+           
+            string file = File.ReadAllText(path);
+            file = file.Replace("\r\n", "\n").Replace("\r", "\n");
 
+            string[] lines = file.Split(new char[] { '\n' }, StringSplitOptions.RemoveEmptyEntries);
+
+            if (lines.Length == 0)
+            {
+                return new string[0, 0];
+            }
+
+            int columns = lines[0].Split(';').Length;
+            int rows = lines.Length;
+
+            string[,] array = new string[rows, columns];
+
+            for (int i = 0; i < rows; i++)
+            {
+                string[] line = lines[i].Split(';');
+
+                for (int j = 0; j < columns; j++)
+                {
+                    if (j < line.Length)
+                    {
+                        array[i, j] = line[j];
+                    }
+                    else
+                    {
+                        array[i, j] = string.Empty;
+                    }
+                }
+            }
+
+            return array;
+        }
     }
 }
--- Tyuiu.KadralinovaAT.Sprint7.Project.V7.Test/DataServiceTest.cs
+++ Tyuiu.KadralinovaAT.Sprint7.Project.V7.Test/DataServiceTest.cs
@@ -5,8 +5,19 @@ namespace Tyuiu.KadralinovaAT.Sprint7.Project.V7.Test
     public sealed class DataServiceTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void ValidGetArray()
         {
+            DataService ds = new DataService();
+            string file = "test.csv";
+            string directory = @"C:\DataSprint7";
+            string path = Path.Combine(directory, file);
+            string[,] res = ds.GetArray(path);
+            Assert.AreEqual("1", res[0, 0]);
+            Assert.AreEqual("2", res[0, 1]);
+            Assert.AreEqual("3", res[1, 0]);
+            Assert.AreEqual("4", res[1, 1]);
+            Assert.AreEqual("5", res[2, 0]);
+            Assert.AreEqual("6", res[2, 1]);
         }
     }
 }
